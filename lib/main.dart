d6dcf74199db611a5f24f6b80d6242f513b05cb4
TheDarkBug <adrianoliviero23@gmail.com>
2023-05-02 23:31:01 +0200

    Added lots of new units

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,14 @@ class App extends StatelessWidget {
 
 class Home extends StatefulWidget {
   const Home({super.key});
-  final String title = "Convert";
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  // String selectedUnit = '';
-  // List<String> units = ['Option 1', 'Option 2', 'Option 3'];
   double value = 0.0;
+  bool isMetric = true;
+  String title = 'Metric to Imperial';
   final TextEditingController controller = TextEditingController();
   @override
   void dispose() {
@@ -37,69 +36,62 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            /*DropdownButtonFormField(
-              value: selectedOption,
-              items: options.map((option) {
-                return DropdownMenuItem(
-                  value: option,
-                  child: Text(option),
-                );
-              }).toList(),
-              onChanged: (newOption) {
-                setState(() {
-                  selectedOption = newOption.toString();
-                });
-              },
-            ),*/
-            TextField(
-              controller: controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Value',
-                hintText: 'e.g. 6.9',
-                suffixIcon: controller.text.isNotEmpty
-                    ? IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () {
-                          setState(() {
-                            controller.clear();
-                            value = 0.0;
-                          });
-                        })
-                    : null,
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                      controller: controller,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: isMetric ? 'Metric' : 'Imperial',
+                        hintText: 'e.g. 6.9',
+                        suffixIcon: controller.text.isNotEmpty
+                            ? IconButton(
+                                icon: const Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    controller.clear();
+                                    value = 0.0;
+                                  });
+                                })
+                            : null,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          this.value =
+                              value.isNotEmpty ? double.parse(value) : 0.0;
+                        });
+                      }),
+                ),
               ),
-              onChanged: (value) {
-                setState(() {
-                  this.value = value.isNotEmpty ? double.parse(value) : 0.0;
-                });
-              },
-              // onSubmitted:
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Switch(
+                    value: isMetric,
+                    onChanged: (value) {
+                      setState(() {
+                        isMetric = value;
+                        title = isMetric
+                            ? 'Metric to Imperial'
+                            : 'Imperial to Metric';
+                      });
+                    }),
+              )
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: getUnits(value, isMetric).length,
+              itemBuilder: (BuildContext context, int index) =>
+                  Center(child: getUnits(value, isMetric)[index]),
             ),
-            SingleChildScrollView(
-              padding: const EdgeInsets.only(top: 40.0),
-              child: Column(
-                children: <Widget>[
-                  Unit(
-                    iSname: 'Kilometer',
-                    mSname: 'Mile',
-                    iSsymbol: 'km',
-                    mSsymbol: 'mi',
-                    value: value,
-                    convert: (src) {
-                      return src * 1.60934;
-                    },
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/unit.dart
+++ lib/unit.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+double round(double value, int places) {
+  num mod = pow(10.0, places);
+  return ((value * mod).round().toDouble() / mod);
+}
 
 class Unit extends StatefulWidget {
-  const Unit(
+  Unit(
       {super.key,
-      required this.iSname,
       required this.mSname,
-      required this.iSsymbol,
+      required this.iSname,
       required this.mSsymbol,
+      required this.iSsymbol,
       required this.value,
-      required this.convert});
-  final String iSname;
+      required this.converted,
+      required this.isMetric});
   final String mSname;
-  final String iSsymbol;
+  final String iSname;
   final String mSsymbol;
-  final double value;
-  final double Function(double) convert;
+  final String iSsymbol;
+  final bool isMetric;
+  double value;
+  double converted;
   @override
   State<Unit> createState() => _UnitState();
 }
@@ -22,51 +30,68 @@ class Unit extends StatefulWidget {
 class _UnitState extends State<Unit> {
   @override
   Widget build(BuildContext context) {
+    widget.value = round(widget.value, 3);
+    widget.converted = round(widget.converted, 3);
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text(
-                      widget.iSname,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
+                Flexible(
+                  child: Column(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          widget.iSname,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
                       ),
-                    ),
-                    Text(
-                      '${widget.value}${widget.iSsymbol}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          '${widget.isMetric ? widget.value : widget.converted}${widget.iSsymbol}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 8.0),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      widget.mSname,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
+                Flexible(
+                  child: Column(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          widget.mSname,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
                       ),
-                    ),
-                    Text(
-                      '${(widget.convert(widget.value) * 1000).roundToDouble() / 1000}${widget.mSsymbol}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          '${widget.isMetric ? widget.converted : widget.value}${widget.mSsymbol}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -75,4 +100,144 @@ class _UnitState extends State<Unit> {
       ),
     );
   }
+}
+
+List<Widget> getUnits(value, isMetric) {
+  return <Widget>[
+    const Padding(
+      padding: EdgeInsets.only(bottom: 20.0),
+      child: Text(
+        'Length',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    ),
+    Unit(
+      iSname: 'Kilometer',
+      mSname: 'Mile',
+      iSsymbol: 'km',
+      mSsymbol: 'mi',
+      value: value,
+      converted: isMetric ? value / 1.60934 : value * 1.60934,
+      isMetric: isMetric,
+    ),
+    Unit(
+      iSname: 'Meter',
+      mSname: 'Yard',
+      iSsymbol: 'm',
+      mSsymbol: 'yd',
+      value: value,
+      converted: isMetric ? value * 1.093613 : value / 1.093613,
+      isMetric: isMetric,
+    ),
+    Unit(
+      iSname: 'Meter',
+      mSname: 'Feet',
+      iSsymbol: 'm',
+      mSsymbol: 'ft',
+      value: value,
+      converted: isMetric ? value * 3.28084 : value / 3.28084,
+      isMetric: isMetric,
+    ),
+    Unit(
+      iSname: 'Centimeter',
+      mSname: 'Inch',
+      iSsymbol: 'cm',
+      mSsymbol: 'in',
+      value: value,
+      converted: isMetric ? value / 2.54 : value * 2.54,
+      isMetric: isMetric,
+    ),
+    const Divider(),
+    const Padding(
+      padding: EdgeInsets.only(bottom: 20.0),
+      child: Text(
+        'Temperature',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    ),
+    Unit(
+      iSname: 'Celsius',
+      mSname: 'Fahrenheit',
+      iSsymbol: '°C',
+      mSsymbol: '°F',
+      value: value,
+      converted: isMetric ? (value * 1.8) + 32 : (value - 32) / 1.8,
+      isMetric: isMetric,
+    ),
+    Unit(
+      iSname: 'Kelvin',
+      mSname: 'Fahrenheit',
+      iSsymbol: 'K',
+      mSsymbol: '°F',
+      value: value,
+      converted: isMetric
+          ? ((value - 273.15) * 1.8) + 32
+          : ((value - 32) / 1.8) + 273.15,
+      isMetric: isMetric,
+    ),
+    const Divider(),
+    const Padding(
+      padding: EdgeInsets.only(bottom: 20.0),
+      child: Text(
+        'Weight',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    ),
+    Unit(
+      iSname: 'Kilogram',
+      mSname: 'Pound',
+      iSsymbol: 'kg',
+      mSsymbol: 'lb',
+      value: value,
+      converted: isMetric ? value / 0.45359237 : value * 0.45359237,
+      isMetric: isMetric,
+    ),
+    Unit(
+      iSname: 'Hectogram',
+      mSname: 'Ounce',
+      iSsymbol: 'g',
+      mSsymbol: 'oz',
+      value: value,
+      converted: isMetric ? value / 2.834952 : value * 2.834952,
+      isMetric: isMetric,
+    ),
+    const Divider(),
+    const Padding(
+      padding: EdgeInsets.only(bottom: 20.0),
+      child: Text(
+        'Volume',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    ),
+    Unit(
+      iSname: 'Liter',
+      mSname: 'US Gallon',
+      iSsymbol: 'l',
+      mSsymbol: 'gal',
+      value: value,
+      converted: isMetric ? value / 3.78541 : value * 3.78541,
+      isMetric: isMetric,
+    ),
+    Unit(
+      iSname: 'Liter',
+      mSname: 'Imperial Gallon',
+      iSsymbol: 'l',
+      mSsymbol: 'gal',
+      value: value,
+      converted: isMetric ? value / 4.54609 : value * 4.54609,
+      isMetric: isMetric,
+    ),
+  ];
 }
