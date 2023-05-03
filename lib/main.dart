a0e99384bc49e6b19999467177b2b6fd39611bd0
TheDarkBug <adrianoliviero23@gmail.com>
2023-05-03 17:07:08 +0200

    Added navigation drawer and settings page (needs implementation)

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,85 @@ void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Unit Converter',
       theme: ThemeData.dark(useMaterial3: true),
-      home: const Home(),
+      home: const HomeScreen(),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
   @override
-  State<Home> createState() => _HomeState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeScreenState extends State<HomeScreen> {
+  Widget _selectedPage = const QuickConvertPage();
+  String _selecctedTitle = "Metric ↔ Imperial";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_selecctedTitle)),
+      body: _selectedPage,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Universal Unit Converter'),
+            ),
+            ListTile(
+              title: const Text('Freedom Units'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _selecctedTitle = "Metric ↔ Imperial";
+                  _selectedPage = const QuickConvertPage();
+                });
+              },
+            ),
+            ListTile(
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _selecctedTitle = "Settings";
+                  _selectedPage = const SettingsPage();
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Settings'));
+  }
+}
+
+class QuickConvertPage extends StatefulWidget {
+  const QuickConvertPage({super.key});
+  @override
+  State<QuickConvertPage> createState() => _QuickConvertPageState();
+}
+
+class _QuickConvertPageState extends State<QuickConvertPage> {
   double value = 0.0;
   bool isMetric = true;
-  String title = 'Metric to Imperial';
   final TextEditingController controller = TextEditingController();
   @override
   void dispose() {
@@ -35,64 +93,58 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                      controller: controller,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        labelText: isMetric ? 'Metric' : 'Imperial',
-                        hintText: 'e.g. 6.9',
-                        suffixIcon: controller.text.isNotEmpty
-                            ? IconButton(
-                                icon: const Icon(Icons.clear),
-                                onPressed: () {
-                                  setState(() {
-                                    controller.clear();
-                                    value = 0.0;
-                                  });
-                                })
-                            : null,
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          this.value =
-                              value.isNotEmpty ? double.parse(value) : 0.0;
-                        });
-                      }),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 20.0),
-                child: Switch(
-                    value: isMetric,
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                    controller: controller,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: isMetric ? 'Metric' : 'Imperial',
+                      hintText: 'e.g. 6.9',
+                      suffixIcon: controller.text.isNotEmpty
+                          ? IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                setState(() {
+                                  controller.clear();
+                                  value = 0.0;
+                                });
+                              })
+                          : null,
+                    ),
                     onChanged: (value) {
                       setState(() {
-                        isMetric = value;
-                        title = isMetric
-                            ? 'Metric to Imperial'
-                            : 'Imperial to Metric';
+                        this.value =
+                            value.isNotEmpty ? double.parse(value) : 0.0;
                       });
                     }),
-              )
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: getUnits(value, isMetric).length,
-              itemBuilder: (BuildContext context, int index) =>
-                  Center(child: getUnits(value, isMetric)[index]),
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Switch(
+                  value: isMetric,
+                  onChanged: (value) {
+                    setState(() {
+                      isMetric = value;
+                    });
+                  }),
+            )
+          ],
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: getUnits(value, isMetric).length,
+            itemBuilder: (BuildContext context, int index) =>
+                Center(child: getUnits(value, isMetric)[index]),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
